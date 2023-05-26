53f3e8dfdbcaff2053a0bb87da11f6a13b7251e9
Aaron Li <a1li@ucsd.edu>
2023-05-26 16:59:36 +0000

    Fix tests and add another

--- tests/abclop99.rs
+++ tests/abclop99.rs
@@ -3,6 +3,12 @@ mod infra;
 success_tests! {
     subdir: "abclop99",
     {
+        name: make_vecs_succ_5,
+        file: "make_vecs.snek",
+        input: "5",
+        expected: "[]\n[1]\n[2, 2]\n[3, 3, 3]\n[4, 4, 4, 4]\n[5, 5, 5, 5, 5]",
+    },
+    {
         name: make_vecs_5_succ_0,
         file: "make_vecs.snek",
         input: "0",
@@ -21,14 +27,21 @@ success_tests! {
         file: "make_vecs.snek",
         input: "2",
         heap_size: 5,
-        expected: "[]\n[1]\n[1, 2]",
+        expected: "[]\n[1]\n[2, 2]",
     },
     {
         name: make_vecs_5_succ_3,
         file: "make_vecs.snek",
         input: "3",
         heap_size: 5,
-        expected: "[]\n[1]\n[1, 2],\n[1, 2, 3]",
+        expected: "[]\n[1]\n[2, 2]\n[3, 3, 3]",
+    },
+    {
+        name: make_vecs_20_succ_5,
+        file: "make_vecs.snek",
+        input: "5",
+        heap_size: 20,
+        expected: "[]\n[1]\n[2, 2]\n[3, 3, 3]\n[4, 4, 4, 4]\n[5, 5, 5, 5, 5]",
     },
 }
 
